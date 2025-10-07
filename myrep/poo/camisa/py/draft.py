20b98c3844825d21dd024079831d4aec9335166c
erica <erica.ryn0304@gmail.com>
2025-10-07 17:25:56 -0300

    ATIVIDADE DA CAMISA - 1 HORA

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -1,4 +1,23 @@
 class Camisa:
 
-    def __init__(self,):
+    def __init__(self):
+        self.__tamanho:str = ""
+
+    def setTamanho(self, valor:str):
+        if valor == "PP" or valor == "P" or valor == "M" or valor == "G" or valor == "GG" or valor == "XG":
+            self.__tamanho   = valor
+        else:
+            print("tamanho invalido")
+
+
+    def getTamanho(self):
+        return self.__tamanho
+        
+camisa = Camisa()
+
+while camisa.getTamanho() == "":
+    print("digite seu tamanho de roupa")
+    tamanho = input()
+    camisa.setTamanho(tamanho)
+print("parabens, voce comprou uma camisa temanho", camisa.getTamanho())
         
